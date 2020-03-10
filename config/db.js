1ccae7a831efb706b269748fecade850fa7b780b
Michelle Van <mjtvan@gmail.com>
2020-03-10 11:20:17 +0000

    added database config

---
+++ config/db.js
@@ -0,0 +1,24 @@
+//mongodb connection 
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+
+//gives us back a promise
+// mongoose.connect(db)
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.log(err.message)
+            //exit process with failure
+            //escape app with failure
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
